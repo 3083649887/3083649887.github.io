5ac21f52e0311301a812c3f85baa06a9dbc2377e
3083649887 <3083649887@qq.com>
2021-05-16 18:06:06 +0800

    sss

--- Js/index.js
+++ Js/index.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
 	$("#menubar").load("Pages/menu.html");
 	
-	var pageid = "home"
-	var pageurl = "null"
+	var pageid = "home";
+	var pageurl = "null";
 	
-	$("#menubar").on("click",function(){
+	$("#menubar").one("click",function(){
 		$(".menus").click(function(){
 			pageid = $(this).attr("id");
 			pageurl = "/Pages/HTMLs/" + pageid + ".html";
 			$("#menubar").off("click");
 		})
 	})
-
+	
+	$("#Hhid").click(function(){
+		$("#Htost").fadeOut(500,function(){
+			$("#hover").fadeOut(1000);
+		});
+	})
 
 })
